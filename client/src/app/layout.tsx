0f0be8696482f239debe1dda28d4360393cf0674
vaden89 <isaacshosanya89@gmail.com>
2026-01-03 00:02:10 +0100

    feat: changed fonts from droid to oswald

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,27 +1,16 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Space_Grotesk, Oswald } from "next/font/google";
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
 
-const droid = localFont({
-  src: "../../public/fonts/droid.otf",
-  display: "swap",
-  variable: "--font-droid",
+const oswald = Oswald({
+  subsets: ["latin"],
+  variable: "--font-oswald",
 });
 
 const siteMetaData = {
@@ -52,9 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}
-          ${spaceGrotesk.variable}
-         ${droid.variable} antialiased`}
+        className={`${spaceGrotesk.variable} ${oswald.variable} font-space-grotesk antialiased`}
       >
         {children}
       </body>
