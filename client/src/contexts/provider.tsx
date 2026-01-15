5b9540ec223bf08b130f4d96b4a0b93c6b2ff1cd
vaden89 <isaacshosanya89@gmail.com>
2026-01-15 23:18:11 +0100

    feat: added themes through next themes

--- client/src/contexts/provider.tsx
+++ client/src/contexts/provider.tsx
@@ -1,11 +1,19 @@
 import ReactLenis from "lenis/react";
+import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
 export default function GlobalProvider({ children }: { children: ReactNode }) {
   return (
     <>
       <ReactLenis root options={{ lerp: 0.2, duration: 1.5 }}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </ReactLenis>
     </>
   );
